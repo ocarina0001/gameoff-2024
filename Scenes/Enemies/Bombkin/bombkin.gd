9f7d41cea94e1d066c9f435477d8ea10788aca4a
ocarina0001 <117322863+ocarina0001@users.noreply.github.com>
2024-11-04 03:30:40 -0500

    basic bombkin script written

---
+++ Scenes/Enemies/Bombkin/bombkin.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+# When we make a sorta generic EnemyComponent for enemies, we will make it interact with this.
+
+# Bombkin's unique behavior is (probably) exploding either when dead OR when close enough to the player
+# I think we should use the explosion image clustered together to make an explosive effect
+
+func _ready() -> void:
+	$AnimatedSprite2D.play("idle")
+
+func _on_explosion_range_body_entered(body: Node2D) -> void:
+	# If we really end up getting advanced, we could use groups and make this
+	# check for "EnemyOfBombkin" or something. but for now "player" is fine.
+	if body.name == "player":
+		$AnimatedSprite2D.play("explode")
+		#$EnemyComponent.can_move = false
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	# This should only happen on 'explode' because it doesn't loop and actually finishes.
+	queue_free()
