26244bdd699d3ce57ee3f8615d8ca27c97f9d255
ocarina0001 <117322863+ocarina0001@users.noreply.github.com>
2024-11-08 22:30:12 -0500

    barely functional room traversal

--- Components/RoomGenerationComponent/room_generation_component.gd
+++ Components/RoomGenerationComponent/room_generation_component.gd
@@ -1,31 +1,30 @@
 extends Node2D
 
+## The list of possible rooms to generate
 @export var rooms_to_generate: Array[PackedScene]
-var level_generation_test: Node2D
-
-# 11/8/2024
-# When a room is picked, we should compare the rotation of the current door 'child'
-# and then find a random door in the new room
-# then rotate the room to make the door "match" the 'child' door
-# and then do the stupid math shit to make the rooms connect
-# and HOPEFULLY that should work.
 
 func _ready() -> void:
-	level_generation_test = get_parent().get_parent()
-
-	for child in get_parent().get_children():
-		if child.is_in_group("door") and !child.has_meta("is_assigned_to_room"):
+	for door in get_parent().get_children():
+		if door.is_in_group("door") and door.is_open:
 			var random_room := rooms_to_generate[int(randi_range(0, rooms_to_generate.size() - 1))]
-			var random_room_instance := random_room.instantiate()
-			var new_room_door: Node2D = null
-			for new_room_child in random_room_instance.get_children():
-				if new_room_child.is_in_group("door") and !new_room_child.has_meta("is_assigned_to_room"):
-					new_room_door = new_room_child
-					child.set_meta("is_assigned_to_room", true)
-					new_room_door.set_meta("is_assigned_to_room", true)
-					break
-
-			if new_room_door != null:
-				var door_offset : Vector2 = child.global_position - new_room_door.global_position + Vector2(0, 32)
-				random_room_instance.position += door_offset
-				level_generation_test.add_child.call_deferred(random_room_instance)
+			var room_instance := random_room.instantiate()
+			
+			var rounded_door_rotation = round(door.rotation_degrees)
+			
+			if rounded_door_rotation == 270 or rounded_door_rotation == -90:
+				pass
+			elif rounded_door_rotation == 90:
+				for child in room_instance.get_children():
+					# Find the appropiate door to connect to
+					if child.is_in_group("door"):
+						child.is_open = true
+						child.connecting_room = door
+						door.connecting_room = child
+						room_instance.position = Vector2(512, 512)
+						break
+			elif rounded_door_rotation == 0:
+				pass
+			elif rounded_door_rotation == 180:
+				pass
+			
+			get_parent().get_parent().add_child.call_deferred(room_instance)
--- Scenes/Decorations/Door/door.gd
+++ Scenes/Decorations/Door/door.gd
@@ -1,8 +1,21 @@
 extends Node2D
 
-## Determines whether or not the door is open.
-@export var is_open: bool = false
+## Determines whether or not the door is open. If the door is open, it connects to another room.
+@export var is_forced_open: bool = false
+var is_open: bool
+var starting_room: Node2D # This gets set to get_parent()
+var connecting_room: Node2D # This gets programatically assigned
 
 func _ready() -> void:
+	starting_room = get_parent()
+	#if randi_range(0, 1) == 0:
+		#is_open = true
+	if is_forced_open:
+		is_open = true
 	if is_open:
 		$DoorFrame.set_cell(Vector2i(0,0), 1, Vector2i(0,0))
+
+func _on_teleporter_body_entered(body: Node2D) -> void:
+	if body.name == "player" and connecting_room != null:
+		print("teleporting")
+		body.position = connecting_room.position
